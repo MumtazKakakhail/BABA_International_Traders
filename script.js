cd58be9ac92ae02e27ace33d5958ff6b05494288
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-04-22 19:04:15 +0500

    BABA International Traders

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const toggleButton = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.nav-links');
+const toggleButton = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
 
-toggleButton.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
+toggleButton.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
 });
